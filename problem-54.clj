e0987090510cb0e2586ede0734bd3c3442c9ab73
Phondanai Khanti <phondanai.khanti@nectec.or.th>
2023-09-11 15:22:52 +0700

    Problem 54

---
+++ problem-54.clj
@@ -0,0 +1,15 @@
+; Problem 54, Partition a Sequence
+(use '[clojure.test])
+
+(defn solve [x col]
+  (let [drp (range 0 (count col) x)]
+    (filter #(= x (count %))
+      (map (fn [y] (take x (drop y col))) drp))))
+
+
+(deftest problem-54
+  (is (solve 3 (range 9)) '((0 1 2) (3 4 5) (6 7 8)))
+  (is (solve 2 (range 8)) '((0 1) (2 3) (4 5) (6 7)))
+  (is (solve 3 (range 8)) '((0 1 2) (3 4 5))))
+
+(print (run-tests))

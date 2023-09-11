194065a2a09c14d05b4b5e7434f9242098c5c073
Phondanai Khanti <phondanai.khanti@nectec.or.th>
2023-09-11 16:14:19 +0700

    Fix missing = in test cases.

--- problem-54.clj
+++ problem-54.clj
@@ -8,8 +8,8 @@
 
 
 (deftest problem-54
-  (is (solve 3 (range 9)) '((0 1 2) (3 4 5) (6 7 8)))
-  (is (solve 2 (range 8)) '((0 1) (2 3) (4 5) (6 7)))
-  (is (solve 3 (range 8)) '((0 1 2) (3 4 5))))
+  (is (= (solve 3 (range 9)) '((0 1 2) (3 4 5) (6 7 8))))
+  (is (= (solve 2 (range 8)) '((0 1) (2 3) (4 5) (6 7))))
+  (is (= (solve 3 (range 8)) '((0 1 2) (3 4 5)))))
 
-(print (run-tests))
+(run-tests)
